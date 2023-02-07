70e01dd6223762b8069e4362ec7068adb504f631
ScribblerCoder <omar2001.oh@gmail.com>
2023-02-07 20:27:18 +0300

    fixed docker volume

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 docker rm container -f cscbot
 docker image rm -f cscbot
 docker build -t cscbot:latest . 
-docker run -d -v data:/app/data --name cscbot -it cscbot:latest
+docker run -d -v $(pwd)/data:/app/data --name cscbot -it cscbot:latest
